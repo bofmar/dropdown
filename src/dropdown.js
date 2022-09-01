7b589f8894b12621036455e449f12899631c2c9a
bofmar <bofmar@outlook.com>
2022-09-01 18:32:57 +0300

    Add logic for the opening and closing of the dropdowns

---
+++ src/dropdown.js
@@ -0,0 +1,19 @@
+export default function dropdown(identifier, menuIdentifier) {
+  document.addEventListener("click", (e) => {
+    const isDropdownButton = e.target.classList.contains(identifier);
+    if (!isDropdownButton && e.target.closest(`.${menuIdentifier}`)) return;
+
+    let currentDropdown;
+    if (isDropdownButton) {
+      console.log(e.target.nextElementSibling);
+      currentDropdown = e.target.nextElementSibling;
+      currentDropdown.classList.toggle("active-dropdown");
+    }
+
+    document.querySelectorAll(".active-dropdown").forEach((item) => {
+      if (item === currentDropdown) return;
+
+      item.classList.remove("active-dropdown");
+    });
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import "./styles/index.scss";
+import dropdown from "./dropdown";
 
 console.log("this works");
+dropdown("droppable","dropdown-menu");
